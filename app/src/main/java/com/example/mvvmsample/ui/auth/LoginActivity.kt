91ae92dc468494756e9e31a09258d5a3d0468759
Antoha1397 <toscha.17@mail.ru>
2019-07-09 23:56:23 +0500

    Add DataBinding + Toast

--- app/src/main/java/com/example/mvvmsample/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/mvvmsample/ui/auth/LoginActivity.kt
@@ -2,12 +2,38 @@ package com.example.mvvmsample.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.get
 import com.example.mvvmsample.R
+import com.example.mvvmsample.util.toast
+import com.example.mvvmsample.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), AuthListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        val binding : ActivityLoginBinding = DataBindingUtil.setContentView(this,R.layout.activity_login)
+        val viewModel = ViewModelProviders.of(this).get(AuthViewModel::class.java)
+        binding.viewmodel=viewModel
+
+        viewModel.authListener=this
     }
+
+    override fun onStarted() {
+             toast("Login Started")
+    }
+
+    override fun onSuccess() {
+        toast("success")
+    }
+
+    override fun onFailure(message: String) {
+                toast(message)
+    }
+
+
 }
