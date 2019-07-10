0e58bfc1d8140c1879d255294542d01848188ad4
Antoha1397 <toscha.17@mail.ru>
2019-07-10 23:10:29 +0500

    Add saveUser in Room

--- app/src/main/java/com/example/mvvmsample/data/repositories/UserRepository.kt
+++ app/src/main/java/com/example/mvvmsample/data/repositories/UserRepository.kt
@@ -1,17 +1,23 @@
 package com.example.mvvmsample.data.repositories
 
 
+import com.example.mvvmsample.data.db.AppDatabase
+import com.example.mvvmsample.data.db.entities.User
 import com.example.mvvmsample.data.network.MyApi
 import com.example.mvvmsample.data.network.SafeApiRequest
 import com.example.mvvmsample.data.network.responses.AuthResponse
 
 import retrofit2.Response
 
-class UserRepository : SafeApiRequest() {
+class UserRepository(private val api: MyApi, private val db:AppDatabase) : SafeApiRequest() {
 
     suspend fun userLogin(email: String, password: String): AuthResponse {
 
-        return apiRequest { MyApi().userLogin(email, password) }
+        return apiRequest { api.userLogin(email, password) }
     }
+
+    suspend fun saveUser(user: User) = db.getUserDao().upsert(user)
+
+    fun getUser() = db.getUserDao().getuser()
 }
 
--- app/src/main/java/com/example/mvvmsample/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/mvvmsample/ui/auth/AuthViewModel.kt
@@ -6,13 +6,14 @@ import com.example.mvvmsample.data.repositories.UserRepository
 import com.example.mvvmsample.util.ApiException
 import com.example.mvvmsample.util.Coroutines
 
-class AuthViewModel : ViewModel() {
+class AuthViewModel(private val repository: UserRepository): ViewModel() {
 
     var email: String? = null
     var password: String? = null
 
     var authListener: AuthListener? = null
 
+    fun getLoggedInUser() = repository.getUser()
     fun onLoginButtonClick(view: View) {
         authListener?.onStarted()
         if (email.isNullOrEmpty() || password.isNullOrEmpty()) {
@@ -23,9 +24,10 @@ class AuthViewModel : ViewModel() {
         Coroutines.main {
 
             try {
-                val authResponse = UserRepository().userLogin(email!!, password!!)
+                val authResponse = repository.userLogin(email!!, password!!)
                 authResponse.user?.let {
                     authListener?.onSuccess(it)
+                    repository.saveUser(it)
                     return@main
                 }
                 authListener?.onFailure(authResponse.message!!)
