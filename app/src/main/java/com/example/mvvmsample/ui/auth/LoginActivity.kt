88c308a921c6c666e0b043fcc5f7daca4fdf418c
Antoha1397 <toscha.17@mail.ru>
2019-07-10 18:35:42 +0500

    Add MyApi + using Retrofit2

--- app/src/main/java/com/example/mvvmsample/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/mvvmsample/ui/auth/LoginActivity.kt
@@ -2,13 +2,17 @@ package com.example.mvvmsample.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.lifecycle.get
 import com.example.mvvmsample.R
 import com.example.mvvmsample.util.toast
 import com.example.mvvmsample.databinding.ActivityLoginBinding
+import com.example.mvvmsample.util.hide
+import com.example.mvvmsample.util.show
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity(), AuthListener {
 
@@ -16,23 +20,27 @@ class LoginActivity : AppCompatActivity(), AuthListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding : ActivityLoginBinding = DataBindingUtil.setContentView(this,R.layout.activity_login)
+        val binding: ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         val viewModel = ViewModelProviders.of(this).get(AuthViewModel::class.java)
-        binding.viewmodel=viewModel
+        binding.viewmodel = viewModel
 
-        viewModel.authListener=this
+        viewModel.authListener = this
     }
 
     override fun onStarted() {
-             toast("Login Started")
+        progress_bar.show()
     }
 
-    override fun onSuccess() {
-        toast("success")
+    override fun onSuccess(loginResponse: LiveData<String>) {
+        loginResponse.observe(this, Observer {
+            progress_bar.hide()
+            toast(it)
+        })
     }
 
     override fun onFailure(message: String) {
-                toast(message)
+        progress_bar.hide()
+        toast(message)
     }
 
 
